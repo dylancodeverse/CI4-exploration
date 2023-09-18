b6d74beddeea050f5fff297826522ad48dac2cdc
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-09-18 20:29:07 +0300

    modified:   app/Models/UserModel.php 	new file:   app/Views/login.php

---
+++ app/Views/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire de Connexion</title>
+</head>
+<body>
+    <h2>Connexion</h2>
+    <form action="process_login.php" method="POST">
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" id="username" name="username" required><br>
+
+        <label for="password">Mot de passe :</label>
+        <input type="password" id="password" name="password" required><br>
+
+        <button type="submit">Se Connecter</button>
+    </form>
+</body>
+</html>
